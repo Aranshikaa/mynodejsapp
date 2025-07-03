b6afec181a47fbc07aac53b1d7c37859d2ff3959
Aranshika kar <aranshikakar@gmail.com>
2025-07-03 14:49:59 +0530

    my first gitpush

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+//const port = 8000;
+
+// Serve static files from the 'public' directory
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to serve a specific HTML file
+app.get('/aranshika', (req, res) => {
+  app.use(express.static(path.join(__dirname, 'public')));
+  res.sendFile((path.join(__dirname, 'public','index.html')));
+});
+app.get('/', (req, res) => {
+  res.write("Hello how are you");
+  res.end();
+});
+app.listen(8000)
+/*
+app.listen(8000, () => {
+  //console.log(`Server listening at http://localhost:${port}`);
+});
+*/
